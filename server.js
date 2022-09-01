41ce74749250da3020e8cc8a6407e28f0f6e75fe
unknown <masummim50@gmail.com>
2022-09-01 16:02:16 +0600

    d

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
+const data = require("./db.json");
 
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./db_videos.json');
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
-const PORT = process.env.PORT || 8000;
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-);
 server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+
+server.listen(port);
